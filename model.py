9f10d5e6cd87b5c2ff4654b6b0650e6bb6b98256
torbjørn saghaug halland <torbjsh@stud.ntnu.no>
2024-03-18 13:45:25 +0100

    Initial model commit

---
+++ model.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+cap = cv2.VideoCapture('fish2.mp4')
+
+# Resize the video window
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Video', 800, 600)  # Adjust the width and height as needed
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    
+    # Preprocessing goes here
+        # Noise reduction: Gaussian blurring and median filtering
+        # Noramlizing and balancing colors, deblur motion blurring
+    
+    # Object detection
+        # HOG
+    
+    # Shape extraction
+        # Background extraction, subtract the last frame
+    
+    # CNN
+        # Khai?
+    
+    
